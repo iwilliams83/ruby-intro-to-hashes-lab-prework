08dd9eee657f02d63aaf247c17a2801a356f74af
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 17:51:59 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,33 +1,3 @@
-=begin
-def new_hash
-
-end
-
-def actor
-
-end
-
-def monopoly
-	monopoly = {}
-
-end
-
-def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-end
-#=====
-=end
 def new_hash
 new_hash = {}
 end
